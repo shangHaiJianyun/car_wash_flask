64c0719b14823c796a8a5427ae58e9b1bec1539c
chendebo <972892867@qq.com>
2018-12-21 09:48:58 +0800

    delete

--- api/common_func/get_role.py
+++
@@ -1,6 +0,0 @@
-from api.models.models import Role
-
-
-def get_user_role(name):
-    user_role = Role.query.filter_by(name=name).first()
-    return user_role
--- api/modules/user/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# 1. 创建蓝图对象
-user_blu = Blueprint("user", __name__, template_folder='templates')
-
-# 4. 关联视图函数
-from .views import *
