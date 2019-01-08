7fa1d38b9d64d1744c09c4ba79a56526df4bc774
chendebo <972892867@qq.com>
2019-01-08 16:08:50 +0800

    修改存储未识别车牌照片的位置

--- api/modules/lpr/views.py
+++ api/modules/lpr/views.py
@@ -13,9 +13,9 @@ import cv2
 def lprM():
     # 读入图片
     f = request.files['file']
-    basepath = os.path.dirname(__file__)  # 当前文件所在路径
+    basepath = sys.path[0]  # 当前文件所在路径
 
-    upload_path = os.path.join(basepath, 'images', secure_filename(f.filename))  # 先将传递的图片存入本地
+    upload_path = os.path.join(basepath + '/api/static/images', secure_filename(f.filename))  # 先将传递的图片存入本地
     f.save(upload_path)
 
     # 使用Opencv转换一下图片格式和名称
