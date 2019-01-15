73c45e222abe7e0899b6088f5be2c1fc5f76fb87
chendebo <972892867@qq.com>
2019-01-15 14:23:03 +0800

    返回数据增加所在区域对应的id

--- api/modules/map/views.py
+++ api/modules/map/views.py
@@ -24,6 +24,7 @@ def get_rate():
             data = {
                 'rate_name': rate['name'],
                 'rate_level': rate['rate_level'],
+                'area_id': j.id,
                 'area_name': j.city_name,
                 'city_code': j.city_code
             }
