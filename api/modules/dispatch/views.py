e29998f6ffd4daaa888df9e2e61d417634392825
chendebo <972892867@qq.com>
2019-03-22 13:08:30 +0800

    back init

--- api/modules/dispatch/views.py
+++ api/modules/dispatch/views.py
@@ -59,54 +59,54 @@ def get_orderlist():
     result = res.json()['data']['data']
     data = []
     for i in result:
-        # service_date = i['service_date']
-        # # print(service_date)
-        # if service_date:
-        #     if compareTime(service_date):
-        #         data.append(i)
-        #         # print(data)
-        order_no = i["order_no"]
-        res = requests.post(
-            url='https://banana.xunjiepf.cn/api/extend/orderDetail',
-            headers={
-                "Content-Type": "application/json"
-            },
-            data=json.dumps({
-                "access_key": access_key,
-                "order_no": order_no
-            })
-        ).json()['data']
-        order_common_info = res['order_common_info']
-        child_list = res['child_list']
-        if len(child_list) > 1:
-            for i in child_list:
-                order_dic = {}
-                service_date = i["service_date"]
-                if compareTime(service_date):
-                    order_dic['order_no'] = i['order_no']
-                    order_dic['order_id'] = i['parent_id']
-                    order_dic['order_status'] = i['order_status']
-                    order_dic['service_date'] = i['service_date']
-                    order_dic['start_time'] = i['start_time']
-                    order_dic['end_time'] = i['end_time']
-                    order_dic['nick_name'] = order_common_info['nick_name']
-                    order_dic['item_title'] = order_common_info['item_title']
-                    order_dic['address_detail'] = order_common_info['address_detail']
-                    data.append(order_dic)
-        else:
-            service_date = child_list[0]["service_date"]
-            order_dic = {}
+        service_date = i['service_date']
+        # print(service_date)
+        if service_date:
             if compareTime(service_date):
-                order_dic['order_no'] = child_list[0]['order_no']
-                order_dic['order_id'] = i["order_id"]
-                order_dic['order_status'] = child_list[0]['order_status']
-                order_dic['service_date'] = child_list[0]['service_date']
-                order_dic['start_time'] = child_list[0]['start_time']
-                order_dic['end_time'] = child_list[0]['end_time']
-                order_dic['nick_name'] = order_common_info['nick_name']
-                order_dic['item_title'] = order_common_info['item_title']
-                order_dic['address_detail'] = order_common_info['address_detail']
-                data.append(order_dic)
+                data.append(i)
+        #         # print(data)
+        # order_no = i["order_no"]
+        # res = requests.post(
+        #     url='https://banana.xunjiepf.cn/api/extend/orderDetail',
+        #     headers={
+        #         "Content-Type": "application/json"
+        #     },
+        #     data=json.dumps({
+        #         "access_key": access_key,
+        #         "order_no": order_no
+        #     })
+        # ).json()['data']
+        # order_common_info = res['order_common_info']
+        # child_list = res['child_list']
+        # if len(child_list) > 1:
+        #     for i in child_list:
+        #         order_dic = {}
+        #         service_date = i["service_date"]
+        #         if compareTime(service_date):
+        #             order_dic['order_no'] = i['order_no']
+        #             order_dic['order_id'] = i['parent_id']
+        #             order_dic['order_status'] = i['order_status']
+        #             order_dic['service_date'] = i['service_date']
+        #             order_dic['start_time'] = i['start_time']
+        #             order_dic['end_time'] = i['end_time']
+        #             order_dic['nick_name'] = order_common_info['nick_name']
+        #             order_dic['item_title'] = order_common_info['item_title']
+        #             order_dic['address_detail'] = order_common_info['address_detail']
+        #             data.append(order_dic)
+        # else:
+        #     service_date = child_list[0]["service_date"]
+        #     order_dic = {}
+        #     if compareTime(service_date):
+        #         order_dic['order_no'] = child_list[0]['order_no']
+        #         order_dic['order_id'] = i["order_id"]
+        #         order_dic['order_status'] = child_list[0]['order_status']
+        #         order_dic['service_date'] = child_list[0]['service_date']
+        #         order_dic['start_time'] = child_list[0]['start_time']
+        #         order_dic['end_time'] = child_list[0]['end_time']
+        #         order_dic['nick_name'] = order_common_info['nick_name']
+        #         order_dic['item_title'] = order_common_info['item_title']
+        #         order_dic['address_detail'] = order_common_info['address_detail']
+        #         data.append(order_dic)
     # print(res.json())
     # return jsonify(res.json()['data']['data'])
     return jsonify(data)
