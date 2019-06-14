fe1682236e1c2ead0b52731da986fc2e6da3eb31
chendebo <972892867@qq.com>
2019-06-14 16:28:24 +0800

    update

--- api/common_func/nearby_area.py
+++ api/common_func/nearby_area.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
 # @Time    : 19-1-11 上午9:47
-# import datetime
-# import time
-#
-#
-# def compareTime(service_date):
-#     time_array = time.strptime(service_date, "%Y-%m-%d")
-#     service_time = time.mktime(time_array)
-#     acquire = datetime.date.today() + datetime.timedelta(days=2)
-#     tomorrow_end_time = int(time.mktime(time.strptime(str(acquire), '%Y-%m-%d'))) - 1
-#     today = datetime.date.today()
-#     today_time = int(time.mktime(today.timetuple()))
-#
-#     # print(service_time, now)
-#     if today_time <= service_time < tomorrow_end_time:
-#         return True
-#     else:
-#         return False
+
 import json
 from math import cos, sin, sqrt, pi, tan, atan2
 
 import requests
 
-from api import NearbyArea
 from api.common_func.area import AreaM, NearbyM
 
 
