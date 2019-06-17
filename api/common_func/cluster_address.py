9af7ec870ae1e9910017be93261ab3069ea6e3ca
chendebo <972892867@qq.com>
2019-06-17 16:17:08 +0800

    update

--- api/common_func/cluster_address.py
+++ api/common_func/cluster_address.py
@@ -10,33 +10,30 @@ from sklearn.datasets.samples_generator import make_blobs
 import numpy as np
 
 
-# Generate sample data
-# centers = [[1, 1], [-1, -1], [1, -1]]
-# X, labels_true = make_blobs(n_samples=200, centers=3, cluster_std=0.5,
-#                             random_state=0)
-
-
 def cluster(X):
+    """
+
+    :param X:np.array([[lat0,lon0],[lat1,lon1],[lat2,lon2]...])
+    :return:labels
+    """
     # Compute Affinity Propagation
     af = AffinityPropagation().fit(X)
-    cluster_centers_indices = af.cluster_centers_indices_
+    # cluster_centers_indices = af.cluster_centers_indices_
     labels = af.labels_
-    n_clusters_ = len(cluster_centers_indices)
-    data = {}
-    try:
-        for k in range(n_clusters_):
-            lump_data = []
-            cluster_center = X[cluster_centers_indices[k]]
-            cen = {'cen': {'lng': cluster_center[0], 'lat': cluster_center[1]}}
-            class_members = labels == k
-            for i in X[class_members].tolist():
-                lump_data.append({'lng': i[0], 'lat': i[1]})
-            lump_data.append(cen)
-            data[str(k)] = lump_data
+    # n_clusters_ = len(cluster_centers_indices)
+    # data = {}
+    # try:
+    #     for k in range(n_clusters_):
+    #         lump_data = []
+    #         # cluster_center = X[cluster_centers_indices[k]]
+    #         # cen = {'cen': {'lng': cluster_center[0], 'lat': cluster_center[1]}}
+    #         class_members = labels == k
+    #         for i in X[class_members].tolist():
+    #             lump_data.append({'lng': i[0], 'lat': i[1]})
+    #         # lump_data.append(cen)
+    #         data[str(k)] = lump_data
 
-        return data
-    except:
-        return {"error": "Unable to clustering"}
+    return labels
 
 
 if __name__ == '__main__':
@@ -51,6 +48,6 @@ if __name__ == '__main__':
         l = [s[n], m[n]]
         n += 1
         list_a.append(l)
-    print(list_a)
+    # print(list_a)
     X = np.array(list_a)
-    cluster(X)
+    print(cluster(X))
