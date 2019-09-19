621e82f8f7167e71fc99102b3cc6162710d07407
chendebo <972892867@qq.com>
2019-09-19 14:39:48 +0800

    update

--- api/common_func/user.py
+++ api/common_func/user.py
@@ -7,6 +7,10 @@ class UserM(object):
     def get(self, id):
         pass
 
+    def get_all_user(self):
+        users = User.query.all()
+        return users
+
     def get_user_by_name(self, name):
         user = User.query.filter_by(username=name).first()
         return user
