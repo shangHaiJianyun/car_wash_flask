7ca837738d2500599ee79f9a80dd24d5629c6417
chendebo <972892867@qq.com>
2019-11-06 13:38:07 +0800

    update

--- api/modules/map/views.py
+++ api/modules/map/views.py
@@ -159,7 +159,8 @@ def judge_active_area():
     unionid = request.json.get('unionid')
     openid = request.json.get('openid')
     address = request.json.get('address')
-    location = json.dumps(dict(lng=lng, lat=lat))
+    bd_lat, bd_lng = convert(lat, lng)
+    location = json.dumps(dict(lng=bd_lng, lat=bd_lat))
     record = SearchRecord(openid=openid, unionid=unionid, locations=location, address=address)
     record.save()
     for j in AreaM().get_all():
